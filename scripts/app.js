3b4a7cb14a711bcbb16ceb1f8bea35419e7d239c
Jose Martin <josemcz2211@gmail.com>
2023-05-22 22:26:16 -0500

    News changeds

--- scripts/app.js
+++ scripts/app.js
@@ -45,7 +45,7 @@ function updateHeaderColors(isScrolled) {
 
   if (isScrolled) {
     header.classList.add("scrolled");
-  } else {  
+  } else {
     header.classList.remove("scrolled");
   }
 }
@@ -53,4 +53,22 @@ function updateHeaderColors(isScrolled) {
 window.addEventListener("scroll", function () {
   const isScrolled = window.scrollY > 0;
   updateHeaderColors(isScrolled);
+});
+
+// ! Creacion de los comentarios
+
+const btnAddComent = document.getElementById("btnComentar");
+const areaComents = document.querySelector(".article_coments-body");
+const coments = document.querySelector(".coment");
+btnAddComent.addEventListener("click", (e) => {
+  e.preventDefault();
+  let spanComent = document.createElement("div");
+  let imgUser = document.createElement("img");
+  imgUser.src = "./../../assets/Component/user.svg";
+  spanComent.appendChild(imgUser);
+  let pComent = document.createElement("label");
+  pComent.innerText = document.formAddComents.txtComent.value;
+  spanComent.appendChild(pComent);
+  coments.appendChild(spanComent);
+  document.formAddComents.txtComent.value = "";
 });
